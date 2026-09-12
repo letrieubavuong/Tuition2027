7dbf89c1d93a2ad980932f221e3c4beee2fde4ff
letrieubavuong <letrieubavuong@gmail.com>
2026-09-13 02:13:09 +0700

    Remove scratch test file

--- scratch/test_import.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:sqflite_common_ffi_web/sqflite_ffi_web.dart';
-
-void main() {
-  if (kIsWeb) {
-    databaseFactory = databaseFactoryFfiWeb;
-  }
-}
